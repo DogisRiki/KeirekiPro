03d3adcc9136600ae715af27766ff040b2d2d2bb
DogisRiki <dogisriki1122@gmail.com>
2024-12-24 23:25:00 +0900

    feat: docker環境構築
    Changes:
    Dockerの各種ファイルの作成及びバックエンドの環境を構築
    
    Reason:
    実装のため
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #4

---
+++ backend/src/main/java/com/example/keirekipro/KeirekiProApplication.java
@@ -0,0 +1,19 @@
+package com.example.keirekipro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 起動エンドポイント
+ */
+@SpringBootApplication
+public class KeirekiProApplication {
+
+    /**
+     * エンドポイント
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(KeirekiProApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/keireki_pro/KeirekiProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keireki_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeirekiProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
