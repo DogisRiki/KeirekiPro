abbbd321c714e8fee16f5a4fffc1cacfbe0eeecc
DogisRiki <dogisriki1122@gmail.com>
2025-01-06 01:40:46 +0900

    front: MUIのベーステーマ作成
    Changes:
    MUIのベーステーマを新規追加
    
    Reason:
    設定を一か所にまとめるため
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #10

---
+++ frontend/src/config/theme.ts
@@ -0,0 +1,38 @@
+import { createTheme } from "@mui/material";
+
+/**
+ * メインカラー
+ */
+const mainColor = "#2C3E50" as const;
+/**
+ * バックグランドカラー
+ */
+const BgColor = "#F5F5F5" as const;
+
+/**
+ * MUI theme
+ */
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: mainColor,
+        },
+        background: {
+            default: BgColor,
+        },
+    },
+    typography: {
+        fontFamily: 'Noto Sans JP, Roboto, "Helvetica Neue", Arial, sans-serif',
+    },
+    components: {
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: mainColor,
+                    },
+                },
+            },
+        },
+    },
+});
