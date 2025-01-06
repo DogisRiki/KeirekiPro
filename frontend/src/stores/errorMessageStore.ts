d6166e0b8a910fea9b6b157c8fde6c42d6fc8a67
DogisRiki <dogisriki1122@gmail.com>
2025-01-06 13:42:27 +0900

    front: エラーメッセージの処理を作成
    Changes:
    ・zustandをインストール
    ・バリデーションエラーをグローバルステートとして管理する処理を追加
    
    Reason:
    エラーメッセージはレスポンスインターセプターとして共通的に処理したいため
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #10

---
+++ frontend/src/stores/errorMessageStore.ts
@@ -0,0 +1,21 @@
+import { ErrorResponse } from "@/types";
+import { create } from "zustand";
+
+interface ErrorMessageState {
+    message: string | null;
+    errors: {
+        [field: string]: string[];
+    };
+    setErrors: (errorResponse: ErrorResponse) => void;
+    clearErrors: () => void;
+}
+
+/**
+ * エラーメッセージを管理するストア
+ */
+export const useErrorMessageStore = create<ErrorMessageState>((set) => ({
+    message: null,
+    errors: {},
+    setErrors: (errorResponse: ErrorResponse) => set({ message: errorResponse.message, errors: errorResponse.errors }),
+    clearErrors: () => set({ message: null, errors: {} }),
+}));
