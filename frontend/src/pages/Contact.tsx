e018eab4e82d90ca39994cc53dbb40e5817ec4de
DogisRiki <dogisriki1122@gmail.com>
2025-01-08 02:36:35 +0900

    front: お問い合わせ画面作成
    Changes:
    ・お問い合わせ画面作成
    ・認証用レイアウトの余白修正
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #10

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,6 +1,35 @@
+import { ContactForm } from "@/features/contact";
+import { Box, Divider, Typography } from "@mui/material";
+
 /**
  * お問い合わせ画面
  */
 export const Contact = () => {
-    return <div>Contact</div>;
+    return (
+        <Box sx={{ maxWidth: 600, mx: "auto", py: 4 }}>
+            {/* ヘッダー部分 */}
+            <Box sx={{ textAlign: "center", mb: 4 }}>
+                <Typography variant="h4" gutterBottom>
+                    お問い合わせ
+                </Typography>
+                <Divider
+                    sx={{
+                        width: 80,
+                        height: 3,
+                        backgroundColor: "primary.main",
+                        mx: "auto",
+                        mt: 1,
+                    }}
+                />
+            </Box>
+            {/* 説明文 */}
+            <Typography variant="body1" gutterBottom sx={{ mb: 4 }}>
+                以下フォームよりお問い合わせください。
+                <br />
+                お問い合わせ内容の確認後、管理人よりご連絡させていただきます。
+            </Typography>
+            {/* お問い合わせフォーム */}
+            <ContactForm />
+        </Box>
+    );
 };
