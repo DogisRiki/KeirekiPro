5e6055e2e35a1b2a00a2e8f123ab3f22e61ef618
DogisRiki <dogisriki1122@gmail.com>
2025-01-07 14:29:24 +0900

    front: Checkboxのラッパーコンポーネント追加
    Changes:
    Checkboxのラッパーコンポーネントを追加
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #10

---
+++ frontend/src/components/ui/Checkbox.tsx
@@ -0,0 +1,8 @@
+import { CheckboxProps, Checkbox as MUICheckbox } from "@mui/material";
+
+/**
+ * チェックボックス
+ */
+export const Checkbox = (props: CheckboxProps) => {
+    return <MUICheckbox {...props} />;
+};
