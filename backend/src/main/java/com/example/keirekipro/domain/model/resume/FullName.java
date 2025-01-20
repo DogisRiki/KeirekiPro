a1dddd27f21323931f1db57dae9a55f9a99bf9de
DogisRiki <dogisriki1122@gmail.com>
2025-01-20 23:07:25 +0900

    add: 氏名(FullName)の値オブジェクトを追加
    Changes:
    氏名(FullName)の値オブジェクトを新規追加
    
    Reason:
    「エラーをまとめて返す」という要件が原因で一時的に値オブジェクト等に不正な値が入ったインスタンスが存在してしまうことになる。
    しかし、ルートエンティティが集約の一貫性を最終保証するという実装で妥協する。
    ルートエンティティ以外から操作できないというしくみを作っ他方がいいとは思う。
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #12

---
+++ backend/src/main/java/com/example/keirekipro/domain/model/resume/FullName.java
@@ -0,0 +1,83 @@
+package com.example.keirekipro.domain.model.resume;
+
+import java.util.Objects;
+
+import com.example.keirekipro.domain.shared.Notification;
+
+import lombok.Getter;
+
+/**
+ * 氏名
+ */
+@Getter
+public class FullName {
+
+    /**
+     * 許可する文字列
+     */
+    private static final String PATTERN = "^[a-zA-Zぁ-んァ-ン一-龯]+$";
+
+    /**
+     * 姓
+     */
+    private final String lastName;
+
+    /**
+     * 名
+     */
+    private final String firstName;
+
+    private FullName(Notification notification, String lastName, String firstName) {
+        validate(notification, lastName, firstName);
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    /**
+     * 生成メソッド
+     *
+     * @param lastName  姓
+     * @param firstName 名
+     * @return 値オブジェクト
+     */
+    public static FullName create(Notification notification, String lastName, String firstName) {
+        return new FullName(notification, lastName, firstName);
+    }
+
+    private void validate(Notification notification, String lastName, String firstName) {
+        if (isInvalidValue(lastName)) {
+            notification.addError("lastName", "姓には英数、ひらがな、カタカナ、漢字のみ使用できます。");
+        }
+        if (isInvalidValue(firstName)) {
+            notification.addError("firstName", "名には英数、ひらがな、カタカナ、漢字のみ使用できます。");
+        }
+    }
+
+    /**
+     * 禁止文字が含まれているかを検証する
+     *
+     * @param lastName  姓
+     * @param firstName 名
+     * @return 検証結果
+     */
+    private boolean isInvalidValue(String field) {
+        return !field.matches(PATTERN);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        FullName fullName = (FullName) obj;
+        return Objects.equals(lastName, fullName.lastName) && Objects.equals(firstName, fullName.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lastName, firstName);
+    }
+}
