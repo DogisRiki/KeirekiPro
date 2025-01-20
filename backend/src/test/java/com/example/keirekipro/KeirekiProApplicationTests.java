55425687c62d463281460bd3a8c1ca178339f84c
DogisRiki <dogisriki1122@gmail.com>
2025-01-20 18:17:09 +0900

    move: テストディレクトリ名の変更
    Changes:
    keireki_pro→keirekiproに変更
    
    Reason:
    警告が出るため
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #12

--- backend/src/test/java/com/example/keirekipro/KeirekiProApplicationTests.java
+++ backend/src/test/java/com/example/keirekipro/KeirekiProApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.keireki_pro;
+package com.example.keirekipro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KeirekiProApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
