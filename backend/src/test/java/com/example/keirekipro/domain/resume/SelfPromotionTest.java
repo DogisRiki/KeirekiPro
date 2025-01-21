1c01f2805612681fa3b0add5354014edff597054
DogisRiki <dogisriki1122@gmail.com>
2025-01-21 18:46:51 +0900

    add: 自己PRエンティティを追加
    Changes:
    自己PRエンティティを新規追加
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #12

---
+++ backend/src/test/java/com/example/keirekipro/domain/resume/SelfPromotionTest.java
@@ -0,0 +1,73 @@
+package com.example.keirekipro.domain.resume;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.example.keirekipro.domain.model.resume.SelfPromotion;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SelfPromotionTest {
+
+    @Test
+    @DisplayName("新規構築用コンストラクタでインスタンス化する")
+    void test1() {
+        SelfPromotion selfPromotion = SelfPromotion.create(0, "タイトル", "自己PRコンテンツ");
+        // インスタンスがnullでない。
+        assertNotNull(selfPromotion);
+        // idが生成されている。
+        assertNotNull(selfPromotion.getId());
+        // 並び順が正しい値である。
+        assertEquals(0, selfPromotion.getOrderNo());
+        // タイトルが正しい値である。
+        assertEquals("タイトル", selfPromotion.getTitle());
+        // コンテンツが正しい値である。
+        assertEquals("自己PRコンテンツ", selfPromotion.getContent());
+    }
+
+    @Test
+    @DisplayName("再構築用コンストラクタでインスタンス化する")
+    void test2() {
+        SelfPromotion selfPromotion = SelfPromotion.reconstruct("1234", 0, "タイトル", "自己PRコンテンツ");
+        // インスタンスがnullでない。
+        assertNotNull(selfPromotion);
+        // idが正しい値である。
+        assertEquals(selfPromotion.getId(), "1234");
+        // 並び順が正しい値である。
+        assertEquals(selfPromotion.getOrderNo(), 0);
+        // タイトルが正しい値である。
+        assertEquals(selfPromotion.getTitle(), "タイトル");
+        // コンテンツが正しい値である。
+        assertEquals(selfPromotion.getContent(), "自己PRコンテンツ");
+    }
+
+    @Test
+    @DisplayName("タイトルを変更する")
+    void test3() {
+        SelfPromotion beforeSelfPromotion = SelfPromotion.create(0, "タイトル", "自己PRコンテンツ");
+        SelfPromotion afterSelfPromotion = beforeSelfPromotion.changeTitle("新しいタイトル");
+        // 変更したタイトルが正しい値である。
+        assertEquals(afterSelfPromotion.getTitle(), "新しいタイトル");
+    }
+
+    @Test
+    @DisplayName("コンテンツを変更する")
+    void test4() {
+        SelfPromotion beforeSelfPromotion = SelfPromotion.create(0, "タイトル", "自己PRコンテンツ");
+        SelfPromotion afterSelfPromotion = beforeSelfPromotion.changeContent("新しい自己PRコンテンツ");
+        // 変更したコンテンツが正しい値である。
+        assertEquals(afterSelfPromotion.getContent(), "新しい自己PRコンテンツ");
+    }
+
+    @Test
+    @DisplayName("タイトル、コンテンツを変更する")
+    void test5() {
+        SelfPromotion beforeSelfPromotion = SelfPromotion.create(0, "タイトル", "自己PRコンテンツ");
+        SelfPromotion afterSelfPromotion = beforeSelfPromotion.changeContent("新しい自己PRコンテンツ")
+                .changeTitle("新しいタイトル");
+        // 変更した項目が正しい値である。
+        assertEquals(afterSelfPromotion.getContent(), "新しい自己PRコンテンツ");
+        assertEquals(afterSelfPromotion.getTitle(), "新しいタイトル");
+    }
+}
