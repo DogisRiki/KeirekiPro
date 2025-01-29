332e05bfacfea5a45e3e45cb9d124fce51c14517
DogisRiki <dogisriki1122@gmail.com>
2025-01-29 23:44:10 +0900

    add: Jwtプロパティファイルを追加
    Changes:
    Jwtプロパティファイルを新規追加
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #16

---
+++ backend/src/main/java/com/example/keirekipro/presentation/security/jwt/JwtProperties.java
@@ -0,0 +1,32 @@
+package com.example.keirekipro.presentation.security.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * JWT設定プロパティファイル
+ */
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
+public class JwtProperties {
+
+    /**
+     * 署名に使用する秘密鍵
+     */
+    private String secret;
+
+    /**
+     * アクセストークンの有効期限
+     */
+    private long accessTokenValidityInMinutes;
+
+    /**
+     * リフレッシュトークンの有効期限
+     */
+    private long refreshTokenValidityInDays;
+}
