ef03878edd2ccf891468edc69484a0f51e24a1e4
DogisRiki <dogisriki1122@gmail.com>
2025-05-14 13:46:12 +0900

    front: [fix]画面遷移時の処理を修正
    Changes:
    画面遷移時にuseLocationのpathnameを見てエラーストアをクリアするように修正
    
    Reason:
    react routerによる画面遷移間でエラーを次ページへ引き継いでしまう現象が発生したため
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #16

--- frontend/src/components/layouts/ProtectedLayout.tsx
+++ frontend/src/components/layouts/ProtectedLayout.tsx
@@ -1,11 +1,20 @@
 import { Footer, MainMenu, UserMenu } from "@/components/ui";
+import { useErrorMessageStore } from "@/stores";
 import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 
 /**
  * ログイン時のみアクセス可能な画面の共通レイアウト
  */
 export const ProtectedLayout = () => {
+    const { clearErrors } = useErrorMessageStore();
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        clearErrors();
+    }, [pathname, clearErrors]);
+
     return (
         <Box>
             <AppBar position="fixed">
