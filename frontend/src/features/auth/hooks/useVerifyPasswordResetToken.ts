8c3dbd5b0778db2b0544398f8f8c13a2d602f372
DogisRiki <dogisriki1122@gmail.com>
2025-05-27 21:59:51 +0900

    front: [fix]useVerifyPasswordResetTokenの型修正
    Changes:
    型の不一致があったため修正
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #16

--- frontend/src/features/auth/hooks/useVerifyPasswordResetToken.ts
+++ frontend/src/features/auth/hooks/useVerifyPasswordResetToken.ts
@@ -1,21 +1,21 @@
 import { verifyPasswordResetToken } from "@/features/auth";
 import { useErrorMessageStore } from "@/stores";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { AxiosError } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 
 /**
  * パスワードリセットトークン検証フック
  * @param token 検証するトークン
  * @returns パスワードリセットトークン検証クエリ結果
  */
-export const useVerifyPasswordResetToken = (token: string): UseQueryResult<void, AxiosError> => {
+export const useVerifyPasswordResetToken = (token: string): UseQueryResult<AxiosResponse<void>, AxiosError> => {
     const { clearErrors } = useErrorMessageStore();
 
-    return useQuery<void, AxiosError>({
+    return useQuery<AxiosResponse<void>, AxiosError>({
         queryKey: ["verifyPasswordResetToken", token],
         queryFn: async () => {
             clearErrors();
-            await verifyPasswordResetToken({ token });
+            return await verifyPasswordResetToken({ token });
         },
         enabled: !!token,
         retry: false,
