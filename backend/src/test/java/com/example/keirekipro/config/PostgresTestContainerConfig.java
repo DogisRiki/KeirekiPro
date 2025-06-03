8c6f6a02c130c7f82d359ce93f556b42f47de312
DogisRiki <dogisriki1122@gmail.com>
2025-06-03 16:49:42 +0900

    fix: PostgresTestContainerConfigのコンテナバージョンを変更
    Changes:
    Dockerfileにあわせて17.2から17.4へ修正
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: #16

--- backend/src/test/java/com/example/keirekipro/config/PostgresTestContainerConfig.java
+++ backend/src/test/java/com/example/keirekipro/config/PostgresTestContainerConfig.java
@@ -18,7 +18,7 @@ public class PostgresTestContainerConfig {
     @ServiceConnection(name = "postgres")
     @SuppressWarnings("resource")
     public PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>("postgres:17.2-alpine")
+        return new PostgreSQLContainer<>("postgres:17.4-alpine")
                 .withDatabaseName("testdb")
                 .withUsername("test")
                 .withPassword("test")
