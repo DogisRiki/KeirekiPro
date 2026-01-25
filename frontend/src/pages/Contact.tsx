51e3f75191b7d8111739972fc1126964273481e6
DogisRiki <dogisriki1122@gmail.com>
2026-01-25 22:44:19 +0900

    front: お問い合わせ画面修正
    Changes:
    準備中のため使用不可に
    
    Reason:
    
    BREAKING CHANGE: N/A
    Related: N/A
    Refs: N/A

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,11 +1,17 @@
 import { Headline } from "@/components/ui";
 import { ContactForm } from "@/features/contact";
 import { Box, Typography } from "@mui/material";
+import { useEffect } from "react";
 
 /**
  * お問い合わせ画面
  */
 export const Contact = () => {
+    useEffect(() => {
+        alert("現在、この機能は準備中です。");
+        window.history.back();
+    }, []);
+
     return (
         <Box sx={{ maxWidth: 600, mx: "auto", py: 4 }}>
             {/* 見出し */}
